cc5e09172c135a5148741b24e93445bb295fd36f
marin-agustin <marinagustin2003@gmail.com>
2024-08-13 01:49:34 -0300

    ejercicio 2 - creacion de clases con sus variables

---
+++ src/ejercicio/dos/Empresa.java
@@ -0,0 +1,19 @@
+package ejercicio.dos;
+
+/**
+ * *****************************************************
+ *
+ * Nombre de la Clase: [Empresa]
+ *
+ * Autor: Agustín Marín Fecha: [13/8/2024]
+ *
+ ******************************************************
+ */
+public class Empresa {
+    
+    //atributos
+    private Empleado empleados[];
+    
+    
+    
+}
